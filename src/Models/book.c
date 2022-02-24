91d44377f6b92082cc68eb0c7894a66220fded2d
Moacir Amaro <moacir@gmail.com>
2022-02-24 15:45:36 -0300

    Add function to get all books

--- src/Models/book.c
+++ src/Models/book.c
@@ -1,1 +1,27 @@
 #include "server.h"
+
+MYSQL_RES *get_books(void)
+{
+	MYSQL_RES *results;
+	MYSQL conn;
+	char	  *query;
+
+	results = NULL;
+	mysql_init(&conn);
+	if (mysql_real_connect(&conn, "localhost", "moacir", "12091997", "labs", 0, NULL, 0))
+	{
+		query = "SELECT books.name, publish_date, author, publisher,categories.name as Category FROM books"
+				" JOIN categories on books.category_code = categories.id";
+		mysql_query(&conn, query);
+		results = mysql_store_result(&conn);
+		mysql_close(&conn);
+	}
+
+	else
+	{
+		printf("Failed to connect to database\n");
+		printf("Error %d: %s\n", mysql_errno(&conn), mysql_error(&conn));
+	}
+	
+	return (results);
+}
